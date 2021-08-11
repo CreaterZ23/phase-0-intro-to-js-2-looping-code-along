f5f2c842b0c8f77e09097210cbab6d157db99d9a
Zeon <zeonstewart23@gmail.com>
2021-08-11 18:38:41 -0400

    commit tree d39ef200799b9556bec860683262567c2dd4c3c8

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solutions in this file
+// node inspect
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; ++i) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+
+
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+
+function writeCards(arrStrings, event) {
+    let newArr = [];
+    for (let i = 0; i < arrStrings.length; i++) {
+        newArr.push(`Thank you, ${arrStrings[i]}, for the wonderful ${event} gift!`)
+
+
+    }
+    return newArr;
+
+}
+
+function countDown() {
+    let i = 10;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+    return i;
+}
